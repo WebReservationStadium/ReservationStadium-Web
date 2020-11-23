214b996319d3554efb03edd2dbb7b2ff5ab9b377
kunlachart promfang <kunlachart.p@ku.th>
2020-11-23 20:44:55 +0700

    delete stadiumall

--- ReservationStadium-Web-master/reservation-web/src/main/java/th/ac/ku/reservation/service/ReservedStadiumService.java
+++ ReservationStadium-Web-master/reservation-web/src/main/java/th/ac/ku/reservation/service/ReservedStadiumService.java
@@ -5,12 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReservedStadiumService {
 
-//    public ReservedStadiumService (ReservedStadiumRepository repository){
-//        this.repository = repository;
-//    }
-//
-//    public void saveState(ReservedStadium reservedStadium){
-//            repository.save(reservedStadium);
-//    }
+
 
 }
